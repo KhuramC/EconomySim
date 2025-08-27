94aa4a4081f9bab21d32ae231445f2bf1629e326
KhuramC <khuramchoudhry13@gmail.com>
2025-08-27 16:10:20 -0500

    fix: change name of poetry project and fix install.sh

--- backend/.devcontainer/features/poetry_install/install.sh
+++ backend/.devcontainer/features/poetry_install/install.sh
@@ -2,5 +2,5 @@
 
 set -e 
 pip install poetry==2.1.4
-poetry config virtualenvs.create True
-poetry config virtualenvs.in-project True
+poetry config virtualenvs.create true
+poetry config virtualenvs.in-project true
