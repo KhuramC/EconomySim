51d2d8bd94cb48c6cb2cfc91c62bdaa19b35a65d
KhuramC <khuramchoudhry13@gmail.com>
2025-08-28 15:21:05 -0500

    docs: add info about attributes of PersonAgent

--- backend/src/backend/engine/agents/person.py
+++ backend/src/backend/engine/agents/person.py
@@ -5,11 +5,19 @@ from .industry import IndustryAgent
 class PersonAgent(Agent):
     """
     An agent representing a person in the simulation.
+    
+    Attributes:
+        income (int): The weekly income of the person.
+        employer (IndustryAgent | None): The industry agent that employs this person, or None if unemployed.
+        current_money (int): The current amount of money the person has; negative indicating debt.
     """
 
-    income: int  # weekly because of timestep
-    employer: IndustryAgent | None  # could be None if unemployed
-    current_money: int  # negative means debt
+    income: int
+    """weekly because of timestep"""
+    employer: IndustryAgent | None
+    """could be None if unemployed"""
+    current_money: int
+    """negative means debt"""
 
     def __init__(
         self,
