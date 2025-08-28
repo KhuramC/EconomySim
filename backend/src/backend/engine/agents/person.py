0ce9c4293f89adf7009aab967e02cf6b702745c4
KhuramC <khuramchoudhry13@gmail.com>
2025-08-28 15:16:33 -0500

    feat: add extra parameters to PersonAgent

--- backend/src/backend/engine/agents/person.py
+++ backend/src/backend/engine/agents/person.py
@@ -1,12 +1,28 @@
-from mesa import Agent
+from mesa import Agent, Model
+from .industry import IndustryAgent
 
 
 class PersonAgent(Agent):
+    """
+    An agent representing a person in the simulation.
+    """
 
-    def __init__(self, model, income: float = 0.0):
+    income: int  # weekly because of timestep
+    employer: IndustryAgent | None  # could be None if unemployed
+    current_money: int  # negative means debt
+
+    def __init__(
+        self,
+        model: Model,
+        income: int = 0,
+        employer: IndustryAgent | None = None,
+        current_money: int = 0,
+    ):
         super().__init__(model)
         self.income = income
+        self.employer = employer
+        self.current_money = current_money
 
     def step(self):
         # behavior of person in each step
-        self.income = self.income + 1.0
+        self.income = self.income + 1
