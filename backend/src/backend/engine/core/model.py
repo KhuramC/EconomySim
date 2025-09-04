2f1690db7b57435a4eea9971bb4d77b5a12d8e47
KhuramC <khuramchoudhry13@gmail.com>
2025-09-04 12:54:58 -0500

    refactor: change initialization of agents to use AgentSets

--- backend/src/backend/engine/core/model.py
+++ backend/src/backend/engine/core/model.py
@@ -1,6 +1,7 @@
 from mesa import Model
 from mesa.datacollection import DataCollector
 
+
 from ..agents.person import PersonAgent
 from ..agents.industry import IndustryAgent
 
@@ -12,25 +13,29 @@ class EconomyModel(Model):
     def __init__(self, num_people=5):
         super().__init__()
         self.week = 0
-        self.people = []
-        self.industries = []
 
-        # Create people
-        for i in range(num_people):
-            person = PersonAgent(model=self, income=100)
-            self.people.append(person)
+        PersonAgent.create_agents(model=self, n=num_people, income=100)
 
         # Create one instance of each industry type
-        for industryType in IndustryType:
-            industry = IndustryAgent(
-                model=self, industry_type=industryType, starting_price=10.0
-            )
-            self.industries.append(industry)
+        IndustryAgent.create_agents(
+            model=self,
+            n=len(IndustryType),
+            industry_type=list(IndustryType),
+            starting_price=10.0,
+        )
 
     def step(self):
         """Advance the simulation by one week."""
         self.week += 1
-        for agent in list(self.industries):
-            agent.step()
-        for agent in list(self.people):
-            agent.step()
+
+        industryAgents = self.agents_by_type[IndustryAgent]
+        for industryAgent in list(industryAgents):
+            industryAgent.step()
+            print(
+                f"Industry Type: {industryAgent.industry_type}, Starting Price: {industryAgent.starting_price}"
+            )
+
+        peopleAgents = self.agents_by_type[PersonAgent]
+        for personAgent in list(peopleAgents):
+            personAgent.step()
+            print("Income:", personAgent.income)
