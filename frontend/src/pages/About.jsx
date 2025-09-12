05b4ce60aa84443421e0027c9cfef633e85f1f83
Micheal <44105295+michealwh@users.noreply.github.com>
2025-09-12 10:30:20 -0500

    about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -14,7 +14,29 @@ import {
 function About() {
 
   return (
-    <h1>About</h1>
+    <Container maxWidth="md">
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="80vh"
+      >
+        <Paper elevation={3} sx={{ p: 4, borderRadius: 3, background: "#416065", color: "white" }}>
+          <h1>About</h1>
+          <Typography variant="body1" color="white">
+            The Economy Simulation is an interactive experience where players
+            explore the dynamics of supply, demand, and resource management.
+            You’ll make decisions, manage resources, and adapt to market
+            conditions in real time. The goal is to balance growth, stability,
+            and sustainability in a competitive environment.
+            Whether you’re curious about economics, testing your strategy skills,
+            or just having fun, this simulation offers a streamlined but
+            challenging environment to learn and play.
+          </Typography>
+        </Paper>
+      </Box>
+    </Container>
   );
 }
 
