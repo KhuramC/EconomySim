17a25cd9fc2e6dc56accbebf4ade3da731ab7528
Micheal <44105295+michealwh@users.noreply.github.com>
2025-09-12 10:47:36 -0500

    about cont

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -21,20 +21,31 @@ function About() {
         justifyContent="center"
         alignItems="center"
         minHeight="80vh"
+        maxWidth="1000px"
+        minWidth="500px"
       >
-        <Paper elevation={3} sx={{ p: 4, borderRadius: 3, background: "#416065", color: "white" }}>
           <h1>About</h1>
-          <Typography variant="body1" color="white">
-            The Economy Simulation is an interactive experience where players
-            explore the dynamics of supply, demand, and resource management.
-            You’ll make decisions, manage resources, and adapt to market
-            conditions in real time. The goal is to balance growth, stability,
-            and sustainability in a competitive environment.
-            Whether you’re curious about economics, testing your strategy skills,
-            or just having fun, this simulation offers a streamlined but
-            challenging environment to learn and play.
+          <Typography variant="h5" color="white" sx={{fontWeight: "bold",paddingBottom: 2, paddingTop: 1}}>
+            Mission Statement
           </Typography>
-        </Paper>
+          <Typography variant="body1" color="white" sx={{paddingLeft: 10, paddingRight: 10}} >
+            As a team, Team JellyBean wants to represent the culmination of our education and knowledge up to this point.
+            The team strives to be as accurate as possible and deliver the best product under our abilities. 
+          </Typography>
+          <Typography variant="h5" color="white" sx={{fontWeight: "bold",paddingBottom: 2, paddingTop: 1}}>
+            Problem Statement
+          </Typography>
+          <Typography variant="body1" color="white" sx={{paddingLeft: 10, paddingRight: 10}}>
+            Many people question the economic decisions of the past and present solely due to their political affiliations. This can lead to people being against decisions simply because it was proposed by a party they don't support.
+            This can be problematic as it leads to society as a whole becoming more closed minded and voting based on affiliation rather than policy and reason.
+          </Typography>
+          <Typography variant="h5" color="white" sx={{fontWeight: "bold",paddingBottom: 2, paddingTop: 1}}>
+            Problem Resolution
+          </Typography>
+          <Typography variant="body1" color="white" sx={{paddingLeft: 10, paddingRight: 10}}>
+            Our solution is a simulation that allows users to change variables that affect a virtual economy. This should decouple economic policies and their perceived political affiliations.
+            Decisions would be destigmatized while allowing the user to have a greater understanding of how an economy works.
+          </Typography>
       </Box>
     </Container>
   );
