31d9cab892c4a5d5397c36245b1b6c9edbd5870d
KhuramC <khuramchoudhry13@gmail.com>
2025-09-12 14:03:57 -0500

    refactor: change starting_prices to price

--- backend/src/backend/engine/agents/industry.py
+++ backend/src/backend/engine/agents/industry.py
@@ -15,19 +15,20 @@ class IndustryAgent(Agent):
 
     industry_type: IndustryType
     """The type of industry this agent represents."""
-    starting_price: float
-    """The initial price of goods/services in this industry."""
+    price: float
+    """The price of goods/services in this industry."""
 
     def __init__(
         self, model: Model, industry_type: IndustryType, starting_price: float = 0.0
     ):
         super().__init__(model)
-        self.starting_price = starting_price
+        self.price = starting_price
         self.industry_type = industry_type
 
     def determine_price(self):
         logging.info("Determining price...NOT IMPLEMENTED")
         # profit maximization from /logic should be called here
+        self.price = self.price + 1
         pass
 
     def change_employment(self):
