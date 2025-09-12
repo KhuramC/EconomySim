36ab30ec1e76be57abaebf3c08faf2c5770b22ee
Evan Schreiner <eschreiner1717@gmail.com>
2025-09-12 23:03:16 +0000

    Added pop up window to edit each topic's parameters.

--- frontend/src/pages/SetupPage.jsx
+++ frontend/src/pages/SetupPage.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import { Box, Grid, Typography, Button } from '@mui/material';
-import {Card, CardContent} from "@mui/material";
+import React, { useState } from 'react';
+import { Box, Grid, Typography, Button, Card, CardContent, Modal } from '@mui/material';
 
 function SetupPage() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [editingTopic, setEditingTopic] = useState(null);
+
+    const handleOpenModal = (topic) => {
+        setEditingTopic(topic);
+        setIsModalOpen(true);
+    }
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setEditingTopic(null);
+    };
+
+
+    console.log("Current topic to edit:", editingTopic);
+
     return (
         // Container with padding
         <Box sx={{ p: 3}}>
@@ -24,7 +39,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Percentage of Pop: 5%, Avg Household Income: $500,000
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Upper Class')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -34,7 +49,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Percentage of Pop: 55%, Avg Household Income: $80,000
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Middle Class')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -44,7 +59,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Percentage of Pop: 40%, Avg Household Income: $30,000
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Lower Class')}>...</Button>
                         </CardContent>
                     </Card>                    
                 </Grid>
@@ -59,7 +74,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Avg. Price of Electricity: $200 / kwH
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Utilities')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -69,7 +84,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Avg. Rent: $500 / month
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Housing')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -79,7 +94,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Avg. Price of Goods: $200
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Groceries')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -89,7 +104,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Avg. Price of Goods: $200
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Entertainment')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -99,7 +114,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Avg. Price of Goods: $800
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Luxury Goods')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -109,7 +124,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Avg. Price Per Gallon: $4.00
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Gas')}>...</Button>
                         </CardContent>
                     </Card>
                 </Grid>
@@ -123,7 +138,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Tax Structure: Progressive Tax
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Taxes')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -133,7 +148,7 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Current rate on imports: 5%
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Tariffs')}>...</Button>
                         </CardContent>
                     </Card>
 
@@ -143,15 +158,60 @@ function SetupPage() {
                             <Typography variant='body2'>
                                 Current Goods Subsidized: Electric cars, food stamps
                             </Typography>
-                            <Button variant='outlined' size='small' sx={{ mt: 1 }}>...</Button>
+                            <Button variant='outlined' size='small' sx={{ mt: 1 }} onClick={() => handleOpenModal('Subsidies')}>...</Button>
                         </CardContent>
                     </Card>
                 </Grid>
             </Grid>
 
+            <Modal open={isModalOpen} onClose={handleCloseModal} aria-labelledby="modal-title">
+                <Box sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 400,
+                    bgcolor: 'background.paper',
+                    border: '2px solid #000',
+                    boxShadow: 24,
+                    p: 4,
+                }}>
+                    <Typography id="modal-title" variant="h6" component="h2" sx={{ color: 'black' }}>Editing {editingTopic}</Typography>
+
+                    {renderModalContent(editingTopic)}
+
+                    <Button onClick={handleCloseModal} sx={{ mt: 2 }}>Save and Close</Button>
+                </Box>
+            </Modal>
+
             <Button variant="contained" color="primary" sx={{ mt: 4 }}>Begin Simulation</Button>
         </Box>
     );
 }
+
+const renderModalContent = (topic) => {
+    switch (topic) {
+        case 'Upper Class':
+        case 'Middle Class':
+        case 'Lower Class':
+            return (
+                <Box>
+                    <Typography sx={{ color: 'black' }}>Adjust Demographics:</Typography>
+                    {/* Add sliders and input fields for demographics here */}
+                </Box>
+            );
+        case 'Utilities':
+        case 'Housing':
+        case 'Entertainment':
+        case 'Groceries':
+        case 'Luxury Goods':
+        case 'Gas':
+        case 'Taxes':
+        case 'Tariffs':
+        case 'Subsidies':
+        default:
+            return <Typography>No settings available for this topic.</Typography>;
+    }
+};
 
 export default SetupPage;
