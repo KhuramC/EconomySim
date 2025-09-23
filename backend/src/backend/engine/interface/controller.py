7604d870cac6dfc4466c1eadac15b1d260d133f4
KhuramC <khuramchoudhry13@gmail.com>
2025-09-23 10:40:41 -0500

    feat: add delete_model and get_indicators

--- backend/src/backend/engine/interface/controller.py
+++ backend/src/backend/engine/interface/controller.py
@@ -91,4 +91,74 @@ class ModelController:
         
         # TODO: engine interfaces:
         # - get econ indicators (choose by time and/or category)
-        
+
+    def delete_model(self, model_id: str) -> None:
+        """
+        Delete the specified model.
+
+        Args:
+            model_id (str): The unique identifier for the model to delete.
+
+        Raises:
+            ValueError: If the model associated with the model_id does not exist.
+        """
+
+        if model_id in self.models:
+            del self.models[model_id]
+        else:
+            raise ValueError(f"Model with ID {model_id} does not exist.")
+
+    def get_indicators(
+        self,
+        model_id: str,
+        start_time: int = 0,
+        end_time: int = 0,
+        indicators: Iterable[str] | None = None,
+    ) -> pd.DataFrame:
+        """
+        Retrieve economic indicators from the specified model.
+
+        Args:
+            model_id (str): The unique identifier for the model to retrieve indicators from.
+            start_time (int): The starting time period for the indicators.
+            end_time (int): The ending time period for the indicators. An end_time of 0 goes to the current time.
+            indicators (Iterable, optional): An interable of specific indicators to retrieve. If None, retrieves all indicators.
+
+        Returns:
+            dataframe (pd.DataFrame): A DataFrame containing the requested economic indicators.
+
+        Raises:
+            ValueError: If the model associated with the model_id does not exist,
+            if the start_time or end_time are invalid,
+            or if one or more requested indicators are not available.
+        """
+
+        # parameter validation
+        if start_time < 0 or end_time < 0 or (end_time != 0 and end_time < start_time):
+            raise ValueError("Invalid start_time or end_time values.")
+        if indicators is not None and not all(
+            ind in available_indicators for ind in indicators
+        ):
+            raise ValueError(
+                f"One or more requested indicators are not available. Available indicators: {available_indicators}"
+            )
+        if model_id in self.models:
+            indicators_df: pd.DataFrame = self.models[
+                model_id
+            ].datacollector.get_model_vars_dataframe()
+
+            # filter by time
+            if end_time == 0:
+                end_time = self.models[model_id].get_week()
+            indicators_df = indicators_df[
+                indicators_df["Week"].between(start_time, end_time)
+            ]
+
+            # filter by indicators
+            if indicators is not None:
+                indicators_df = indicators_df[list(indicators)]
+
+            return indicators_df
+
+        else:
+            raise ValueError(f"Model with ID {model_id} does not exist.")
