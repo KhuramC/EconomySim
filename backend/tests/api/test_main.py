26aa4aca7637b047bb2a4ec460c8367ab4cff600
KhuramC <khuramchoudhry13@gmail.com>
2025-10-28 03:04:44 +0000

    move websocket tests to its own file

--- backend/tests/api/test_main.py
+++ backend/tests/api/test_main.py
@@ -7,7 +7,6 @@ from typing import Any
 from api.city_template import CityTemplate
 from engine.types.demographic import Demographic
 from engine.types.indicators import Indicators
-import json
 import copy
 
 template_test_params = [
@@ -179,37 +178,3 @@ def test_step_model(api_client: TestClient, created_model: int):
     # Failure path
     response = api_client.post("/models/999/step")
     assert response.status_code == status.HTTP_404_NOT_FOUND
-
-
-def test_model_websocket(api_client: TestClient, created_model: int):
-    """
-    Test for `step_model_websocket`, an API endpoint.
-    Tests that it can connect, simulating a possible use case and works as expected.
-    Args:
-        api_client (TestClient): the test client to connect to the FastAPI server.
-        created_model (int): the id of the model created.
-    """
-
-    with api_client.websocket_connect(f"/models/{created_model}") as websocket:
-        # 1. Send a "step" action and verify the response
-        websocket.send_json({"action": "step"})
-        response_step = websocket.receive_json()
-        assert response_step == {"status": "success", "action": "step"}
-
-        # 2. Send a "get_indicators" action and verify the response
-        websocket.send_json({"action": "get_indicators"})
-        response_indicators = websocket.receive_json()
-
-        # Assert the structure of the indicators response
-        assert response_indicators["status"] == "success"
-        assert response_indicators["action"] == "get_indicators"
-        assert "data" in response_indicators
-
-        # Assert the content of the indicators data
-        indicators_data = response_indicators["data"]
-        assert isinstance(indicators_data, dict)
-        for indicator_name, indicator_data in indicators_data.items():
-            # although week is in the data, it's not an indicator
-            if indicator_name != "week":
-                assert indicator_name in Indicators.values()
-            assert len(indicator_data) == 1
