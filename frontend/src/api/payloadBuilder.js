68ea7ee698c1c89576a52eaad761db3487af5982
schreiner <eschreiner1717@gmail.com>
2025-11-05 04:21:28 +0000

    Obtain inflation as an annual whole percent, but store as a weekly decimal percent

--- frontend/src/api/payloadBuilder.js
+++ frontend/src/api/payloadBuilder.js
@@ -55,7 +55,7 @@ export function buildEnvironmentPayload(envParams) {
   return {
     max_simulation_length: envParams.maxSimulationLength,
     num_people: envParams.numPeople,
-    inflation_rate: envParams.inflationRate / 100, // e.g., 1.0% -> 0.01
+    inflation_rate: envParams.inflationRate / 100 / 52, // Convert annual % to a weekly rate
     random_events: envParams.randomEvents,
   };
 }
