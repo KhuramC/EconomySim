4f63063861380b5aef878c58e26d35d1bfbf4d0c
MistaSistaJustin <justinsills2003@gmail.com>
2025-11-12 00:57:45 +0000

    sales tax and income tax tests pass

--- backend/tests/engine/agents/test_person.py
+++ backend/tests/engine/agents/test_person.py
@@ -32,6 +32,7 @@ def test_payday(mock_economy_model, income: int):
         mock_economy_model: a mock model
         income (int): the income of the agent.
     """
+    mock_economy_model.policies["personal_income_tax"]['middle class'] = 0
     starting_money = 0
     person = PersonAgent(
         mock_economy_model,
@@ -41,8 +42,28 @@ def test_payday(mock_economy_model, income: int):
         starting_balance=starting_money,
     )
     person.payday()
-    assert person.balance == (starting_money + income - (income * mock_economy_model.policies["personal_income_tax"]['middle class']))
+    assert person.balance == (starting_money + income)
+@mark.parametrize("personal_income_tax, expected_balance", [(1,0), (0.02,490), (0.1,450), (1/3,333.333333333), (0,500)])
+def test_income_tax(mock_economy_model, personal_income_tax, expected_balance):
+    """
+    Test for `payday` with varying levels of income tax.
+    Tests that after a payday, the current money has correctly increased.
 
+    Args:
+        mock_economy_model: a mock model
+        income (int): the income of the agent.
+    """
+    mock_economy_model.policies["personal_income_tax"]['middle class'] = personal_income_tax
+    starting_money = 0
+    person = PersonAgent(
+        mock_economy_model,
+        Demographic.MIDDLE_CLASS,
+        preferences={},
+        income=500,
+        starting_balance=starting_money,
+    )
+    person.payday()
+    assert person.balance == approx(expected_balance)
 
 @mark.parametrize(
     "income, savings_rate, expected_budget",
@@ -96,14 +117,17 @@ def test_demand_func(mock_economy_model):
     assert demands[IndustryType.ENTERTAINMENT] == approx(60.0)
     assert demands[IndustryType.GROCERIES] == approx(16.0)
 
-
-def test_purchase_goods(mock_economy_model):
+@mark.parametrize(
+    "food_sales_tax, entertainment_sales_tax, purchased_food_inventory, purchased_entertainment_inventory, total_spent",
+    [(0.1, 0.15,29,21,802), (1, 0.5,16,16,800), (1/3, 5/7,24,14,800), (0, 0,32,24,800)],
+)
+def test_purchase_goods(mock_economy_model, food_sales_tax, entertainment_sales_tax, purchased_food_inventory, purchased_entertainment_inventory, total_spent):
     """
     Tests the full `purchase_goods` cycle when all desired items are in stock.
     """
 
     # TODO: Implement testing for when desired items are NOT available (inventory too low).
-
+    mock_economy_model.policies["sales_tax"][IndustryType.GROCERIES] = food_sales_tax
     food_industry = IndustryAgent(
         mock_economy_model,
         IndustryType.GROCERIES,
@@ -111,6 +135,8 @@ def test_purchase_goods(mock_economy_model):
         starting_inventory=100,
     )
     food_industry.inventory_available_this_step = food_industry.inventory
+    
+    mock_economy_model.policies["sales_tax"][IndustryType.ENTERTAINMENT] = entertainment_sales_tax
     entertainment_industry = IndustryAgent(
         mock_economy_model,
         IndustryType.ENTERTAINMENT,
@@ -128,14 +154,30 @@ def test_purchase_goods(mock_economy_model):
     )
 
     person.purchase_goods()
-
+    
+    # 1. Payday: Current money becomes 1000. Budget is 800.
+    # 2. GROCERY: 800 * 0.4 = 320. Quantity: 320 / (10 * 1.1) ~= 29, actually spent 11 * 29 = 319.
+    # 3. ENTERTAINMENT: 800 * 0.6 = 480. Quantity: 480 / (20*1.15) ~= 21, actually spent 23 * 21 = 483.
+    # 4. Total spent: 319 + 460 = 802
+    
+    # 1. Payday: Current money becomes 1000. Budget is 800.
+    # 2. GROCERY: 800 * 0.4 = 320. Quantity: 320 / (10 * 2) = 16, actually spent 20 * 16 = 320.
+    # 3. ENTERTAINMENT: 800 * 0.6 = 480. Quantity: 480 / (20* 1.5) = 16, actually spent 30 * 16 = 480.
+    # 4. Total spent: 320 + 480 = 800
+    
+    # 1. Payday: Current money becomes 1000. Budget is 800.
+    # 2. GROCERY: 800 * 0.4 = 320. Quantity: 320 / (10 * (1 + 1/3)) = 24, actually spent ~13.33333 * 24 = 320.
+    # 3. ENTERTAINMENT: 800 * 0.6 = 480. Quantity: 480 / (20 * (1 + 5/7)) = 14, actually spent ~34.2857 * 14 = 480.
+    # 4. Total spent: 320 + 480 = 800
+    
     # 1. Payday: Current money becomes 1000. Budget is 800.
     # 2. GROCERY: 800 * 0.4 = 320. Quantity: 320 / 10 = 32.
     # 3. ENTERTAINMENT: 800 * 0.6 = 480. Quantity: 480 / 20 = 24.
     # 4. Total spent: 800
-    assert food_industry.inventory == 100 - 32
-    assert entertainment_industry.inventory == 100 - 24
-    assert person.balance == approx(200)
+    assert total_spent == approx((purchased_food_inventory * (10 * (1 + food_sales_tax))) + (purchased_entertainment_inventory * (20 * (1 + entertainment_sales_tax))))
+    assert food_industry.inventory == approx(100 - purchased_food_inventory)
+    assert entertainment_industry.inventory == approx(100 - purchased_entertainment_inventory)
+    assert person.balance == approx(1000 - total_spent)
 
 
 @mark.xfail(reason="Function not implemented yet.")
