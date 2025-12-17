26140fd463b215f9221a7e622b9dc003c730f53e
KhuramC <khuramchoudhry13@gmail.com>
2025-12-17 17:59:19 +0000

    fix double import

--- backend/engine/agents/person.py
+++ backend/engine/agents/person.py
@@ -1,5 +1,4 @@
 from mesa import Agent, Model
-from .demand import demand_func
 from .industry import IndustryAgent
 from .demand import demand_func
 from ..types.demographic import Demographic, DEMOGRAPHIC_SIGMAS
